dd518a3ca6415fb465e31bd10a1298d08307db95
Ashok Bhati <ashokbhati763@gmail.com>
2021-02-18 16:59:43 +0530

    initialization

---
+++ api/models/Post.js
@@ -0,0 +1,39 @@
+/**
+ * Post.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    postContent: {
+      type: "string",
+      columnType: "longtext",
+      required: true,
+    },
+    featureImage: {
+      type: "string",
+      columnType: "longtext",
+    },
+    tags: {
+      type: "string",
+    },
+    isPublished: {
+      type: "boolean",
+      defaultsTo: false,
+    },
+    publishedAt: {
+      type: "date",
+    },
+    categories: {
+      collection: "category",
+      via: "posts",
+    },
+  },
+};
+
